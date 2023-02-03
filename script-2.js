b1404ae32283a507d5d8a354262f15a9b0a5f289
Abdulganiu <80805865+Kalejaiye75@users.noreply.github.com>
2023-02-03 17:40:44 +0000

    Added a login form Modified the login form with Js Added styles for the login form

--- script-2.js
+++ script-2.js
@@ -4,3 +4,24 @@ function footerNote() {
     footerNote.textContent = `CopyWrite ${new Date().getFullYear()} @gani`;
 }
 footerNote();
+const titleField = () => {
+    let title = document.getElementById('login-field');
+title.innerHTML = "Login";
+};
+titleField();
+
+let loginForm = document.getElementById("login")
+    loginForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        alert("You are now logged in")
+        let email = document.getElementById("emaill");
+        let passWord = document.getElementById("password");
+        
+        if (email.value == "" || passWord.value == "") {
+           alert("Please Input your login details"); 
+        } else {
+            alert("You are now logged in");
+            console.log(`This user's login details is ${email.value} and ${passWord.value}`)
+        }
+        
+    }); 
